a136b2b8b24202340b468f7fdb32c0ed89091576
RamiAlkhateeb <rami13195@gmail.com>
2020-03-30 13:07:42 +0300

    with Material Library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Material2Module } from './material2.module';
+
+import {HttpClientModule , HttpClient} from '@angular/common/http';
+
+import {TranslateModule , TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http:HttpClient){
+  return new TranslateHttpLoader(http)
+}
+
 
 @NgModule({
   declarations: [
@@ -10,7 +24,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    Material2Module,
+    FlexLayoutModule,
+    //DirectivesModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader:{
+        provide:TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps :[HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material2.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatCardModule } from '@angular/material/card'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatListModule } from '@angular/material/list'
+import { MatTooltipModule } from '@angular/material/tooltip'
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatTableModule } from '@angular/material/table'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatDialogModule } from '@angular/material/dialog'
+
+  
+@NgModule({
+
+  imports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatDividerModule, MatIconModule, 
+            MatSidenavModule, MatToolbarModule, MatListModule, MatTooltipModule, MatTabsModule, 
+            MatTableModule, MatFormFieldModule, MatInputModule, MatDialogModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatDividerModule, MatIconModule, 
+            MatSidenavModule, MatToolbarModule, MatListModule, MatTooltipModule, MatTabsModule, 
+            MatTableModule, MatFormFieldModule, MatInputModule, MatDialogModule]
+})
+export class Material2Module { }
